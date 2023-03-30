c1eb817d27678665a337ae48f291a330c7f82b21
tokziqijocelyn <jocelyntok10.21@ichat.sp.edu.sg>
2023-03-31 01:47:07 +0800

    best model hire him

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { useState } from 'react';
+import Start from './imageOver'
+
+
+function App() {
+
+  return (
+    <div className="App" >
+      <header className="App-header">
+        <h1>Why does Jun Foong suck</h1>
+        <Start />
+      </header>
+    </div>
+  );
+}
+
+export default App;
